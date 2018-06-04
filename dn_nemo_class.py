2f5674b3aaf1cd35276c12c02fb40a3622aed15b
asd3427 <34855528+asd3427@users.noreply.github.com>
2018-06-04 21:40:22 +0800

    更新

--- dn_nemo_class.py
+++ dn_nemo_class.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 import os 
 import struct
-
+import cv2
 
 def load_mnist(path, kind='train'):
     """Load MNIST data from `path`"""
@@ -29,10 +29,10 @@ def load_mnist(path, kind='train'):
     return images, labels
 
 #讀取訓練數據集
-image_train_int8,labels_train = load_mnist(r'D:\github\dn_nemo_class\Mnist',kind='train')
+image_train_int8,labels_train = load_mnist(r'E:\GIT_hub\dn_nemo_class\Mnist',kind='train')
 
 #讀取測試數據
-image_test_int8,labels_test = load_mnist(r'D:\github\dn_nemo_class\Mnist',kind='t10k')
+image_test_int8,labels_test = load_mnist(r'E:\GIT_hub\dn_nemo_class\Mnist',kind='t10k')
 
 thresh = 50
 image_train = (image_train_int8>=thresh)*1
@@ -53,3 +53,79 @@ ax[0].set_xticks([])
 ax[0].set_yticks([])
 plt.tight_layout()
 plt.show()
+
+
+
+from sklearn.ensemble import RandomForestClassifier
+import datetime
+
+'''
+1.指定一个模型
+2.给数据 根据输入数据 以选择的模型基础上进行训练
+3.使用训练好的模型进行预测
+'''
+model=  RandomForestClassifier(n_estimators=50,n_jobs=-1)
+starttime  = datetime.datetime.now()
+model.fit(image_train,labels_train)
+endtime = datetime.datetime.now()
+timetrain = endtime-starttime
+print(timetrain)
+
+
+
+# 读取图片
+im   = cv2.imread(r'E:\GIT_hub\dn_nemo_class\4\3.png')
+
+#将图片转为灰阶
+im_gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+
+#重置图片大小
+
+im_final = cv2.resize(im_gray,(28,28))
+
+thresg = 30
+im_bi = (im_final>=thresh)*1
+im_vec = im_bi.flatten()
+im_vec = im_vec.reshape(1,-1)
+
+ #使用训练好得模型进行预测
+ 
+prediciton = model.predict(im_vec)
+ 
+print ('the num of the img is :'+ str(prediciton))
+
+cv2.imshow('666',im)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+'''
+1.读取图片
+2.转化为图片格式
+3.预处里
+4.向量化
+5.进模型
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
